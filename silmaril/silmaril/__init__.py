7940f6d193d411e98474108e9f22179f47a98e73
LanceM28 <lmoreau@lmoreau@terpmail.umd.edu>
2026-05-07 18:30:42 -0500

    Adding silmaril

---
+++ silmaril/silmaril/__init__.py
@@ -0,0 +1,7 @@
+"""Package for simulating gravitationally lensed observations of high-redshift galaxies"""
+__all__ = ["galaxy","imaging","lens","utilities"]
+from silmaril import *
+from silmaril.utilities import *
+from silmaril.imaging import *
+from silmaril.galaxy import *
+from silmaril.lens import *
